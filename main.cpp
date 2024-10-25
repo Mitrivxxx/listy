d38bbe67095837da61a80fbc39f78c311c768f9b
Mateusz <matsma2468@gmail.com>
2024-10-25 14:11:16 +0200

    drogi commit

--- main.cpp
+++ main.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
-
+#include <list>
 using namespace std;
+
+template <class T>
+struct Node {
+    T data;
+    Node<T>* prev;
+    Node<T>* next;
+
+    //kompilator wie ze nie moze byc uzywany niejawnie, tylko jawne przekazywanie
+    explicit Node(T value) : data(value), prev(nullptr), next(nullptr) {}
+};
+class DoublyLinkedList{
+private:
+    int counter;
+public:
+    Node<T>* head;
+    Node<T>* tail;
+
+    DoublyLinkedList()=default;
+
+};
 
 int main()
 {
-    cout << "Hello world!" << endl;
+
+
     return 0;
 }
